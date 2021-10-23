3ec1f2087be1cfd001966152a070306fc15ebbfc
Martin Sun <yize.sun@campus.lmu.de>
2021-10-23 19:35:32 +0200

    apply gridworld methods

--- workingFile.py
+++ workingFile.py
@@ -41,134 +41,6 @@ class State:
     def __str__(self):
         return f"State(cat_pos={self.catP})"
 
-# agent: cat
-class Cat:
-    def __init__(self, eps, qTable, gridWorld, training):
-        self.eps = eps
-        #self.qCircuit = 
-        # this is a dict
-        self.qt = qTable
-        self.gw = gridWorld
-        self.params = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
-        # quantum circuit
-        self.backend = Aer.get_backend("qasm_simulator")
-        self.NUM_SHOTS = 1000 # (get_counts = [500, 500])
-
-        # optimizer
-        self.optimizer = COBYLA(maxiter=500, tol=0.0001)
-        self.training = training
-
-        # result: ret = optimizer.optimize() 
-        # self.rets = {(0,0):ret, (0,1):ret2,...}
-        
-        # we have 9 circuits here in qcs TODO: maybe a random, need try, need solve!!!
-        self.qcs = self.initQC(ret[0])
-
-        # for updating our qTable
-        self.qc = None
-        self.state = None
-    
-    def initQC(self, params):
-        qcs = {}
-        def qcMaker(params):
-            qr = QuantumRegister(2, name="q")
-            cr = ClassicalRegister(2, name="c")
-            qc = QuantumCircuit(qr, cr)
-            qc.u3(params[0], params[1], params[2], qr[0])
-            qc.u3(params[3], params[4], params[5], qr[1])
-            # qc.cx(qr[0], qr[1]) cnot gate
-            # qc.u3(params[6], params[7], params[8], qr[0])
-            # qc.u3(params[9], params[10], params[11], qr[1])
-            qc.measure(qr, cr)
-            return qc
-
-        for i in range(len(self.gw)):
-            for j in range(len(self.gw[i])):
-                qc = qcMaker(params)
-                qcs[i, j] = qc
-        return qcs
-
-    def selectAction(self, state):
-        if random.uniform(0, 1) < self.eps: # exploration
-            return random.choice(ACTIONS)
-        else: # greedy 
-            if self.training:
-                # [0,0]
-                self.qc = self.qcs[state.row, state.column]
-                self.state = state
-                self.updateCircuit(state)
-            return np.argmax(self.qt[state])
-
-    def lossFunction(self, params):
-        state = self.state
-        qc = self.qc
-        t_qc = transpile(qc, self.backend)
-        job = assemble(t_qc, shots=self.NUM_SHOTS)
-        rlt = self.backend.run(job).result()
-        counts = rlt.get_counts(qc)
-        action = max(counts, key = counts.get)
-        nextPosition = self.newPosition(state, action) # handle the 
-        reward, _ = self.getReward(nextPosition)
-        # update q-table(but not very sure, update only for this action or for all actions)
-        targetQvalue = reward + gamma *  np.max(self.qt[State(nextPosition)])
-        if targetQvalue - self.qt[state][action] > 0:
-            self.qt[state][action] += alpha * (targetQvalue - self.qt[state][action]) # update q-table
-        return targetQvalue - self.qt[state][action]
-
-    def newPosition(self, state, action):
-            p = deepcopy(state.catP)
-            if action == UP:
-                p[0] = max(0, p[0] - 1)
-            elif action == DOWN:
-                p[0] = min(len(self.gw) - 1, p[0]+1)
-            elif action == LEFT:
-                p[1] = max(0, p[1] - 1)
-            elif action == RIGHT:
-                p[1] = min(len(self.gw) - 1, p[1] + 1)
-            else:
-                raise ValueError(f"Unkown action {action}")
-            return p
-
-    def getReward(self, p):
-        grid = self.gw[p[0]][p[1]]
-        if grid == DOG:
-            reward = -100
-            end = True
-            self.gw[p[0]][p[1]] += CAT
-        elif grid == MOUSE:
-            reward = 100
-            end = True
-            self.gw[p[0]][p[1]] += CAT
-        elif grid == EMPTY:
-            reward = -1
-            end = False
-            old = state.catP
-            self.gw[old[0]][old[1]] = EMPTY
-            self.gw[p[0]][p[1]] = CAT
-        elif grid == CAT:
-            reward = -2 # (maybe less than reward of empty)
-            end = False
-        else:
-            raise ValueError(f"Unknown grid item {grid}")
-        return reward, end
-
-    def act(self, state, action):
-        p = self.newPosition(state, action)
-        reward, end = self.getReward(p)
-        return deepcopy[p], reward, end
-        
-    def updateCircuit(self, state):
-        self.rets[state] = self.optimizer.optimize(num_vars=6, objective_function=self.lossFunction, initial_point=self.params)
-    
-    def setTraining(self, training):
-        self.Training = training
-
-# quantum circuit: state->action
-class QCircuit:
-    pass
-
-#####################################################################################################
-#new part
 class GridWorld:
     def __init__(self, s, catP, mouseP):
         self.numRows = s[0]
@@ -233,6 +105,133 @@ class GridWorld:
                     output += EMPTY + " "
             output += "\n"
         print(output)
+
+# agent: cat
+class Cat:
+    def __init__(self, eps, qTable, gridWorld:GridWorld, training):
+        self.eps = eps
+        #self.qCircuit = 
+        # this is a dict
+        self.qt = qTable
+        self.gw = gridWorld
+        self.params = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+        # quantum circuit
+        self.backend = Aer.get_backend("qasm_simulator")
+        self.NUM_SHOTS = 1000 # (get_counts = [500, 500])
+
+        # optimizer
+        self.optimizer = COBYLA(maxiter=500, tol=0.0001)
+        self.training = training
+
+        # result: ret = optimizer.optimize() 
+        # self.rets = {(0,0):ret, (0,1):ret2,...}
+        
+        # we have 9 circuits here in qcs TODO: maybe a random, need try, need solve!!!
+        self.qcs = self.initQC(ret[0])
+
+        # for updating our qTable
+        self.qc = None
+        self.state = None
+    
+    def initQC(self, params):
+        qcs = {}
+        def qcMaker(params):
+            qr = QuantumRegister(2, name="q")
+            cr = ClassicalRegister(2, name="c")
+            qc = QuantumCircuit(qr, cr)
+            qc.u3(params[0], params[1], params[2], qr[0])
+            qc.u3(params[3], params[4], params[5], qr[1])
+            # qc.cx(qr[0], qr[1]) cnot gate
+            # qc.u3(params[6], params[7], params[8], qr[0])
+            # qc.u3(params[9], params[10], params[11], qr[1])
+            qc.measure(qr, cr)
+            return qc
+
+        for i in range(self.gw.getNumRows()):
+            for j in range(self.gw.getNumColumns()):
+                qc = qcMaker(params)
+                qcs[i, j] = qc
+        return qcs
+
+    def selectAction(self, state):
+        if random.uniform(0, 1) < self.eps: # exploration
+            return random.choice(ACTIONS)
+        else: # greedy 
+            if self.training:
+                # [0,0]
+                self.qc = self.qcs[state.row, state.column]
+                self.state = state
+                self.updateCircuit(state)
+            return np.argmax(self.qt[state])
+
+    def lossFunction(self, params):
+        state = self.state
+        qc = self.qc
+        t_qc = transpile(qc, self.backend)
+        job = assemble(t_qc, shots=self.NUM_SHOTS)
+        rlt = self.backend.run(job).result()
+        counts = rlt.get_counts(qc)
+        action = max(counts, key = counts.get)
+        nextPosition = self.newPosition(state, action) # handle the 
+        reward, _ = self.getReward(nextPosition)
+        # update q-table(but not very sure, update only for this action or for all actions)
+        targetQvalue = reward + gamma *  np.max(self.qt[State(nextPosition)])
+        if targetQvalue - self.qt[state][action] > 0:
+            self.qt[state][action] += alpha * (targetQvalue - self.qt[state][action]) # update q-table
+        return targetQvalue - self.qt[state][action]
+
+    def newPosition(self, state, action):
+            p = deepcopy(state.catP)
+            if action == UP:
+                p[0] = max(0, p[0] - 1)
+            elif action == DOWN:
+                p[0] = min(self.gw.getNumRows() - 1, p[0]+1)
+            elif action == LEFT:
+                p[1] = max(0, p[1] - 1)
+            elif action == RIGHT:
+                p[1] = min(self.gw.getNumColumns() - 1, p[1] + 1)
+            else:
+                raise ValueError(f"Unkown action {action}")
+            return p
+
+    def getReward(self, p):
+        grid = self.gw.getItem(p)
+        if grid == DOG:
+            reward = -100
+            end = True
+            self.gw.setCatP(p)
+        elif grid == MOUSE:
+            reward = 100
+            end = True
+            self.gw.setCatP(p)
+        elif grid == EMPTY:
+            reward = -1
+            end = False
+            self.gw.setCatP(p)
+        elif grid == CAT:
+            reward = -2 # (maybe less than reward of empty)
+            end = False
+        else:
+            raise ValueError(f"Unknown grid item {grid}")
+        return reward, end
+
+    def act(self, state, action):
+        p = self.newPosition(state, action)
+        reward, end = self.getReward(p)
+        return deepcopy[p], reward, end
+        
+    def updateCircuit(self, state):
+        self.rets[state] = self.optimizer.optimize(num_vars=6, objective_function=self.lossFunction, initial_point=self.params)
+    
+    def setTraining(self, training):
+        self.Training = training
+
+# quantum circuit: state->action
+class QCircuit:
+    pass
+
+#####################################################################################################
+#new part
                     
 
 class PetSchool:
@@ -308,7 +307,7 @@ def initqTable(self, ACTIONS):
         return d
 # initGridWorld
 gridWorld = GridWorld(gridSize, catP=catP, mouseP=mouseP)
-cat = Cat()
+
 
 
 
